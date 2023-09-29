4e8ce938d85dd0386a373a940a7f689cd030e97a
levdalba <91618084+levdalba@users.noreply.github.com>
2023-09-29 15:34:45 +0700

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -1,26 +1,3 @@
-# def count_duplicates(word):
-#     char_count = {}
-#     duplicates = 0
-
-#     for char in word:
-#         char_lower = char.lower()
-
-#         if char_lower.isalpha():
-#             if char_lower in char_count:
-#                 char_count[char_lower] += 1
-#                 if char_count[char_lower] == 2:
-#                     duplicates += 1
-#             else:
-#                 char_count[char_lower] = 1
-
-#     return duplicates
-
-
-# word = input("enter the word: ")
-# result = count_duplicates(word)
-
-# print(f"{word} - {result} duplicates")
-
 def count_duplicates(word):
     count = {}
     duplicates = 0
